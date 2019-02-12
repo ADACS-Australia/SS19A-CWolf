299403e397d7f83bbccb89e7b9f54d667c67034a
Lewis Lakerink <llakerink@swin.edu.au>
2019-02-12 01:51:05 +0000

    Initial work on the detailed view. Tidied up the sidebar and file upload a little

--- src/Components/MarzApp.jsx
+++ src/Components/MarzApp.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./Sidebar.jsx";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Usage from "./Pages/Usage.jsx";
 import Footer from "./Footer.jsx";
+import Detailed from "./Pages/Detailed.jsx";
 
 function MarzApp(props) {
     return (
@@ -19,6 +20,7 @@ function MarzApp(props) {
                         <div className="spacing relative">
                             <Route exact path="/" render={(routeProps) => <Overview {...props} {...routeProps}/>}/>
                             <Route path="/usage/" render={(routeProps) => <Usage {...props} {...routeProps}/>}/>
+                            <Route path="/detailed/" render={(routeProps) => <Detailed {...props} {...routeProps}/>}/>
                         </div>
                     </div>
                 </div>
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import Dropzone from "react-dropzone";
+
+import styled from 'styled-components';
+
+const getColor = (props) => {
+    if (props.isDragReject) {
+        return '#c66';
+    }
+    if (props.isDragActive) {
+        return '#5E99FF';
+    }
+    return '#bbb';
+};
+
+const Container = styled.div`
+  padding: 10px;
+  width: 100%;
+  border-width: 4px;
+  border-radius: 5px;
+  border-color: ${props => getColor(props)};
+  border-style: ${props => props.isDragReject || props.isDragActive ? 'solid' : 'dashed'};
+  background-color: ${props => props.isDragReject || props.isDragActive ? '#eee' : ''};
+  text-align: center;
+`;
 
 class Sidebar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.onDrop.bind(this)
+    }
+
+    onDrop(acceptedFiles, rejectedFiles) {
+        // Do something with files
+        console.log(acceptedFiles, rejectedFiles)
     }
 
     render() {
@@ -10,24 +41,24 @@ class Sidebar extends React.Component {
             <div className="filler light-grey-background">
                 <div className="spacing">
                     <div id="sidebar-wrapper">
-                        <div id="sidebarDrop">
-                            dropzone
-                            !(showSave() && ui.sidebarSmall)
-                            <div>
-                                isWaitingDrop()
-
-                                <h4>getDropText()</h4>
-                            </div>
-                            <div className="capitalise">
-                                !isWaitingDrop()
-                                <h4>getTitle()</h4>
-                            </div>
-                            <span className="btn btn-default btn-file">
-                                Browse
-                                <input type="file" id="files" name="files[]"/>
-                            </span>
-
-                        </div>
+                        <Dropzone onDrop={Sidebar.onDrop}>
+                            {({getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject, acceptedFiles}) => {
+                                return (
+                                    <Container
+                                        isDragActive={isDragActive}
+                                        isDragReject={isDragReject}
+                                        {...getRootProps()}
+                                    >
+                                        <input {...getInputProps()} />
+                                        <h4 className="file-drop-h5">{isDragAccept ? 'Drop' : 'Drag'} a FITs file or a
+                                            results file. Or drop a results file and THEN a FITs file.</h4>
+                                        <span className="btn btn-light btn-file">
+                                            Browse <input {...getInputProps()}/>
+                                        </span>
+                                    </Container>
+                                )
+                            }}
+                        </Dropzone>
                         {/*<div className="top-spacing" ng-if="showSave()">*/}
                         {/*<button className="btn btn-block btn-sm btn-default"*/}
                         {/*ng-click="toggleSmall()">{{getContractButtonLabel()}}</button>*/}
@@ -43,79 +74,76 @@ class Sidebar extends React.Component {
                         {/*</div>*/}
                         {/*</div>*/}
                         {/*</div>*/}
+                        <div className="top-spacing">
+                            <b>Display Data As:</b>
+                            <div className="btn-group btn-group-justified">
+                                <div className="btn-group">
+                                    <button type="button" className="btn btn-light">
+                                        Graph
+                                    </button>
+                                </div>
+                                <div className="btn-group">
+                                    <button type="button" className="btn btn-light">
+                                        Table
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                         {/*<div className="top-spacing" ng-if="showTabular()">*/}
-                        {/*<b>Display Data As:</b>*/}
-                        {/*<div className="btn-group btn-group-justified">*/}
-                        {/*<div className="btn-group">*/}
-                        {/*<button type="button" className="btn btn-default " ng-click="ui.graphicalLayout=true"*/}
-                        {/*ng-className="{active:ui.graphicalLayout}">Graph*/}
-                        {/*</button>*/}
-                        {/*</div>*/}
-                        {/*<div className="btn-group">*/}
-                        {/*<button type="button" className="btn btn-default " ng-click="ui.graphicalLayout=false"*/}
-                        {/*ng-className="{active:!ui.graphicalLayout}">Table*/}
-                        {/*</button>*/}
-                        {/*</div>*/}
-                        {/*</div>*/}
-                        {/*</div>*/}
-                        {/*<div className="top-spacing" ng-if="showTabular()">*/}
-                        {/*<b>Filter Spectra:</b>*/}
-                        {/*<select className="input-sm form-control" ng-model="filters['typeFilter']" required*/}
-                        {/*ng-options="opt.value as opt.label for opt in types"></select>*/}
-                        {/*<select className="input-sm form-control" ng-model="filters['templateFilter']" required*/}
-                        {/*ng-options="opt.value as opt.label for opt in temps"></select>*/}
-                        {/*<select className="input-sm form-control" ng-model="filters['redshiftFilter']" required*/}
-                        {/*ng-options="opt.value as opt.label for opt in redshifts"></select>*/}
-                        {/*<select className="input-sm form-control" ng-model="filters['qopFilter']" required*/}
-                        {/*ng-options="opt.value as opt.label for opt in qops"></select>*/}
-                        {/*</div>*/}
-                        {/*<div ng-if="showSave()" className="top-spacing">*/}
+                        <b>Filter Spectra:</b>
+                        <select className="input-sm form-control" required></select>
+                        <select className="input-sm form-control" required></select>
+                        <select className="input-sm form-control" required></select>
+                        <select className="input-sm form-control" required></select>
+                    </div>
+                    {/*<div ng-if="showSave()" className="top-spacing">*/}
                         {/*<b>Save QOP:</b>*/}
                         {/*<button className="btn btn-block btn-sm btn-success" ng-click="saveManual(4)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(4)}}</button>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(4)}}</button>*/}
                         {/*<button className="btn btn-block btn-sm btn-info" ng-click="saveManual(3)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(3)}}</button>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(3)}}</button>*/}
                         {/*<button className="btn btn-block btn-sm btn-warning" ng-click="saveManual(2)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(2)}}</button>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(2)}}</button>*/}
                         {/*<button className="btn btn-block btn-sm btn-danger" ng-click="saveManual(1)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(1)}}</button>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(1)}}</button>*/}
                         {/*<button className="btn btn-block btn-sm btn-primary" ng-click="saveManual(6)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(6)}}</button>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(6)}}</button>*/}
                         {/*<button className="btn btn-block btn-sm btn-default" ng-click="saveManual(0)" tooltip="Saved!"*/}
-                        {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
-                        {/*tooltip-placement="right">{{getButtonLabel(0)}}</button>*/}
-                        {/*</div>*/}
-                        {/*<div className="top-spacing" ng-if="!showSave() || !ui.sidebarSmall">*/}
-                        {/*<b>Stepping options:</b>*/}
-                        {/*<div className="checkbox checkbox-info checkbox-circle" style="margin-left: 5px;">*/}
-                        {/*<input type="checkbox" id="checkbox1" ng-model="ui.detailed.onlyQOP0">*/}
-                        {/*<label for="checkbox1">*/}
-                        {/*QOP 0 only*/}
-                        {/*</label>*/}
-                        {/*</div>*/}
+                                {/*tooltip-append-to-body="true" tooltip-trigger="focus"*/}
+                                {/*tooltip-placement="right">{{getButtonLabel(0)}}</button>*/}
+                    {/*</div>*/}
+                    <div className="top-spacing">
+                        <b>Stepping options:</b>
+                        <div className="checkbox checkbox-info checkbox-circle">
+                            <input type="checkbox" id="checkbox1"/>
+                                <label>
+                                    QOP 0 only
+                                </label>
+                        </div>
                         {/*</div>*/}
                     </div>
-                    {/*<div ng-if="!showSave() || !ui.sidebarSmall" id="sidebar-list" className="top-spacing lined scroll-y"*/}
-                    {/*resize="windowResized" ng-style="listStyle()">*/}
-                    {/*<div ng-click="setActive(spectra)" ng-dblclick="goToDetailed(spectra)"*/}
-                    {/*ng-repeat="spectra in data.spectra" className="left-padding-small spectra-list-item"*/}
-                    {/*ng-className="{activeSelect:isActive(spectra)}" ng-class-even="'darker'">*/}
-                    {/*<p>*/}
-                    {/*<strong className="right-padding-small">{{spectra.id}}</strong>{{getAnalysedText(spectra)}}*/}
-                    {/*<span ng-show="spectra.hasRedshift()"*/}
-                    {/*className="pull-right label {{spectra.qopLabel}}">{{spectra.qop}}</span>*/}
-                    {/*</p>*/}
-                    {/*</div>*/}
-                    {/*</div>*/}
+                    <div id="sidebar-list" className="top-spacing lined scroll-y">
+                        todo
+                        {/*<div className="left-padding-small spectra-list-item">*/}
+                            {/*<p>*/}
+                                {/*<strong className="right-padding-small">{{*/}
+                                    {/*spectra*/}
+                                    {/*.id*/}
+                                {/*}}</strong>{{getAnalysedText(spectra)}}*/}
+                                {/*<span ng-show="spectra.hasRedshift()"*/}
+                                      {/*className="pull-right label {{spectra.qopLabel}}">{{spectra.qop}}</span>*/}
+                            {/*</p>*/}
+                        {/*</div>*/}
+                    </div>
                 </div>
             </div>
-        )
+    )
     }
-}
+    }
 
-export default Sidebar
+    export default Sidebar
